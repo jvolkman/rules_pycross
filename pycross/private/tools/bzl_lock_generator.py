f982dc8cac83c15c954c67d8aafc00c6c1f6c8b9
Jeremy Volkman <jeremy@jvolkman.com>
2022-03-23 22:26:14 -0700

    Generate package deps

--- pycross/private/tools/bzl_lock_generator.py
+++ pycross/private/tools/bzl_lock_generator.py
@@ -5,6 +5,7 @@ import argparse
 import json
 import os
 import sys
+import textwrap
 
 from packaging.requirements import Requirement
 from packaging.version import Version
@@ -31,7 +32,7 @@ class Package:
 
 
 class PdmMetadata:
-    def __init__(self, requirements, packages_by_name, links):
+    def __init__(self, requirements: List[Requirement], packages_by_name: Dict[str, List[Package]]):
         self._requirements = requirements
         self._packages_by_name = packages_by_name
 
@@ -78,21 +79,118 @@ class PdmMetadata:
                 )
             )
 
-        return cls(requirements, packages_by_name, [])
+        return cls(requirements, packages_by_name)
 
 
-@dataclass
-class Entry:
-    package_name: str
-    package_deps_by_target: Dict[str, str]
+class PackageTargetBuilder:
+    def __init__(self, package_name: str, prefix: str):
+        self.prefix = prefix
+        self.package_name = package_name
+        self.common_deps: Set[Requirement] = set()
+        self.env_deps: Dict[str, Set[Requirement]] = {}
+
+    def set_dependencies(self, dependencies: List[Requirement], target_environments: List[TargetEnv]) -> None:
+        env_deps = defaultdict(list)
+        for dep in dependencies:
+            for target in target_environments:
+                if not dep.marker or dep.marker.evaluate(target.markers):
+                    target_label = environment_target_name(target, self.prefix)
+                    env_deps[target_label].append(dep)
+
+        if env_deps:
+            # Pull out deps common to all environments
+            common_deps = set.intersection(*(set(v) for v in env_deps.values()))
+            env_deps_deduped = {}
+            for env, deps in env_deps.items():
+                deps = set(deps) - common_deps
+                if deps:
+                    env_deps_deduped[env] = deps
+
+            self.env_deps = env_deps_deduped
+            self.common_deps = common_deps
 
     def __str__(self):
-        return (
-            f'Entry(\n    name = "{self.package_name}"\n    deps={self.package_deps_by_target}\n)'
-        )
+        def ind(text: str, tabs=1):
+            return textwrap.indent(text, "    " * tabs)
+
+        lines = [
+            "Entry(",
+            ind(f'name = "{self.package_name}",'),
+        ]
+
+        def dep_entries(deps, indent):
+            return [ind(requirement_target_name(d, self.prefix), indent) + "," for d in deps]
+
+        if self.common_deps and self.env_deps:
+            lines.append(ind("deps = [", 1))
+            lines.extend(dep_entries(self.common_deps, 2))
+            lines.append(ind("] + select({", 1))
+            for env, deps in self.env_deps.items():
+                lines.append(ind(f'"{env}": [', 2))
+                lines.extend(dep_entries(deps, 3))
+                lines.append(ind("],", 2))
+            lines.append(ind("}),", 1))
+
+        elif self.common_deps:
+            lines.append(ind("deps = [", 1))
+            lines.extend(dep_entries(self.common_deps, 2))
+            lines.append(ind("],", 1))
+
+        elif self.env_deps:
+            lines.append(ind("deps = select({", 1))
+            for env, deps in self.env_deps.items():
+                lines.append(f'"{environment_target_name(env, self.prefix)}": [', 2)
+                lines.extend(dep_entries(deps, 3))
+                lines.append(ind("],", 2))
+            lines.append(ind("}),", 1))
+
+        lines.append(")")
+
+        return "\n".join(lines)
+
+
+def requirement_target_name(requirement: Requirement, prefix: str) -> str:
+    return prefix + "_" + requirement.name.lower().replace("-", "_")
+
+
+def environment_target_name(requirement: Requirement, prefix: str) -> str:
+    return prefix + "_" + requirement.name.lower().replace("-", "_")
 
 
 def main():
+    prefix = "foo_"
+    parser = make_parser()
+    args = parser.parse_args()
+    output = args.output
+    targets = []
+    for target_file in args.target_environment_file or []:
+        with open(target_file, "r") as f:
+            targets.append(TargetEnv.from_dict(json.load(f)))
+
+    metadata = PdmMetadata.create(args.pdm_project_file, args.pdm_lock_file)
+
+    work = list(metadata.get_requirements())
+
+    entries = {}
+    while work:
+        next_req = work.pop()
+        pkg_name = next_req.name.lower()
+        if pkg_name in entries:
+            continue
+        package = metadata.get_package(next_req)
+        work.extend(package.dependencies)
+
+        entries[pkg_name] = PackageTargetBuilder(package_name=pkg_name, prefix=prefix)
+        entries[pkg_name].set_dependencies(package.dependencies, targets)
+
+    entry_list = sorted(entries.values(), key=lambda x: x.package_name)
+    with open(output, "w") as f:
+        for e in entry_list:
+            print(e, file=f)
+            print(file=f)
+
+
+def make_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="Generate pycross dependency bzl file."
     )
@@ -125,46 +223,7 @@ def main():
         help="The path to the output bzl file.",
     )
 
-    args = parser.parse_args()
-    output = args.output
-    targets = []
-    for target_file in args.target_environment_file or []:
-        with open(target_file, "r") as f:
-            targets.append(TargetEnv.from_dict(json.load(f)))
-
-    metadata = PdmMetadata.create(args.pdm_project_file, args.pdm_lock_file)
-
-    work = list(metadata.get_requirements())
-
-    entries = {}
-    while work:
-        next_req = work.pop()
-        pkg_name = next_req.name.lower()
-        if pkg_name in entries:
-            continue
-        package = metadata.get_package(next_req)
-        work.extend(package.dependencies)
-
-        deps_by_target = defaultdict(list)
-        for dep in package.dependencies:
-            for target in targets:
-                if not dep.marker or dep.marker.evaluate(target.markers):
-                    deps_by_target[target.python_compatible_with].append(dep)
-
-        if deps_by_target:
-            common_deps = set.intersection(*(set(v) for v in deps_by_target.values()))
-            deps_by_target = {k: [d for d in v if d not in common_deps] for k, v in deps_by_target.items()}
-            deps_by_target[None] = common_deps
-
-
-        # TODO: handle platform/extra crap
-        entries[pkg_name] = Entry(package_name=pkg_name, package_deps_by_target=deps_by_target)
-
-    entry_list = sorted(entries.values(), key=lambda x: x.package_name)
-    with open(output, "w") as f:
-        for e in entry_list:
-            print(e, file=f)
-            print(file=f)
+    return parser
 
 
 if __name__ == "__main__":
