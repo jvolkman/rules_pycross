22a53d63a5d123388c8cf1317ff98224629fea17
Jeremy Volkman <jeremy@jvolkman.com>
2022-03-28 13:29:13 -0700

    Pull in partial extract_wheels from rules_python

---
+++ pycross/private/tools/extract_wheels/README.md
@@ -0,0 +1,3 @@
+## Wheel extraction
+Adapted from rules_python: 
+https://github.com/bazelbuild/rules_python/tree/main/python/pip_install/extract_wheels/lib
---
+++ pycross/private/tools/extract_wheels/extract.py
@@ -0,0 +1,63 @@
+"""Utility functions to manipulate Bazel files"""
+from pathlib import Path
+from typing import Optional
+from typing import Union
+
+from pycross.private.tools.extract_wheels import (
+    namespace_pkgs,
+    purelib,
+    wheel,
+)
+
+
+def setup_namespace_pkg_compatibility(wheel_dir: Union[str, Path]) -> None:
+    """Converts native namespace packages to pkgutil-style packages
+
+    Namespace packages can be created in one of three ways. They are detailed here:
+    https://packaging.python.org/guides/packaging-namespace-packages/#creating-a-namespace-package
+
+    'pkgutil-style namespace packages' (2) and 'pkg_resources-style namespace packages' (3) works in Bazel, but
+    'native namespace packages' (1) do not.
+
+    We ensure compatibility with Bazel of method 1 by converting them into method 2.
+
+    Args:
+        wheel_dir: the directory of the wheel to convert
+    """
+
+    namespace_pkg_dirs = namespace_pkgs.implicit_namespace_packages(
+        wheel_dir,
+        ignored_dirnames=["%s/bin" % wheel_dir],
+    )
+
+    for ns_pkg_dir in namespace_pkg_dirs:
+        namespace_pkgs.add_pkgutil_style_namespace_pkg_init(ns_pkg_dir)
+
+
+def extract_wheel(
+    wheel_file: str,
+    enable_implicit_namespace_pkgs: bool,
+    directory: Optional[Path] = None,
+) -> None:
+    """Extracts wheel into given directory.
+
+    Args:
+        wheel_file: the filepath of the .whl
+        enable_implicit_namespace_pkgs: if true, disables conversion of implicit namespace packages and will unzip as-is
+        directory: The directory to extract into.
+
+    Returns:
+        The Bazel label for the extracted wheel, in the form '//path/to/wheel'.
+    """
+
+    whl = wheel.Wheel(wheel_file)
+    if directory is None:
+        directory = Path(".")
+
+    whl.unzip(directory)
+
+    # Note: Order of operations matters here
+    purelib.spread_purelib_into_root(directory)
+
+    if not enable_implicit_namespace_pkgs:
+        setup_namespace_pkg_compatibility(directory)
