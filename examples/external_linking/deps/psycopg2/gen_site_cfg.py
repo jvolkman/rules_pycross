33b36a6be1cf9aaab19e9a97ee4174a7ea6f1a3e
Jeremy Volkman <jeremy@jvolkman.com>
2022-12-14 19:38:36 -0800

    wip

---
+++ examples/external_linking/deps/psycopg2/gen_site_cfg.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+
+cwd_path = Path(os.getcwd())
+sdist_path = Path(os.environ["PYCROSS_SDIST_ROOT"])
+openblas_lib = Path(os.environ["OPENBLAS_LIB"])
+openblas_include = Path(os.environ["OPENBLAS_INCLUDE"])
+
+include_dir = cwd_path / openblas_include
+lib_dir = cwd_path / openblas_lib.parent
+
+p = sdist_path / "openblas"
+p.mkdir()
+(p / "include").symlink_to(include_dir, target_is_directory=True)
+(p / "lib").symlink_to(lib_dir, target_is_directory=True)
+
+site_cfg = f"""\
+[openblas]
+libraries = openblas
+library_dirs = openblas/lib
+include_dirs = openblas/include
+search_static_first = 1
+extra_link_args = -lm
+"""
+
+with open(sdist_path / "site.cfg", "w") as f:
+    f.write(site_cfg)
