jvolkman/rules_pycross

commit aaa6873cd0ede9219ae054cc0b29a31a1f555713

Rewrite the PDM translator to not depend on PDM
PDM is a heavyweight dependency that pulls in ~27 transitive deps,
including `msgpack` which is not pure-Python. We don't need all of that
just to translate the lock file.

pycross/private/tools/pdm_translator.py
@@ -1,17 +1,21 @@
+from __future__ import annotations
+
 import os
+import re
+from collections import defaultdict
+from dataclasses import dataclass
 from pathlib import Path
-from tempfile import TemporaryDirectory
 from typing import Any
 from typing import Dict
 from typing import List
+from typing import Set
+from urllib.parse import urlparse
 
+import tomli
+from packaging.requirements import Requirement
+from packaging.specifiers import SpecifierSet
 from packaging.utils import NormalizedName
-from packaging.utils import Version
-from pdm.cli.filters import GroupSelection as PDMGroupSelection
-from pdm.core import Core as PDMCore
-from pdm.exceptions import PdmUsageError
-from pdm.models.candidates import Candidate as PDMCandidate
-from pdm.project import Project as _PDMProject
+from packaging.version import Version
 
 from pycross.private.tools.args import FlagFileArgumentParser
 from pycross.private.tools.lock_model import LockSet
@@ -30,167 +34,245 @@ class LockfileNotStaticException(Exception):
     pass
 
 
-class PDMProject(_PDMProject):
-    def __init__(self, project_file: Path, lock_file: Path, cache_dir: Path) -> None:
-        super().__init__(
-            core=PDMCore(),
-            root_path=project_file.parent.absolute(),
-        )
-        if not self.lockfile.is_compatible():
-            raise LockfileIncompatibleException("Lock file is not compatible with PDM version used by pycross.")
+class MismatchedVersionException(Exception):
+    pass
 
-        if "static_urls" not in self.lockfile.strategy:
-            raise LockfileNotStaticException(
-                "Lock file does not contain static urls. " "Please use --static-urls when creating the lockfile."
+
+# We support anything in the 4.x range. At least that's the idea.
+SUPPORTED_LOCK_VERSIONS = SpecifierSet("~=4.0")
+EDITABLE_PATTERN = re.compile("^ *-e +")
+
+
+def get_default_dependencies(lock: Dict[str, Any]) -> List[Requirement]:
+    deps = lock.get("project", {}).get("dependencies", [])
+    return [Requirement(dep) for dep in deps]
+
+
+def get_optional_dependencies(lock: Dict[str, Any]) -> Dict[str, List[Requirement]]:
+    dep_groups = lock.get("project", {}).get("optional-dependencies", {})
+    return {group: [Requirement(dep) for dep in deps] for group, deps in dep_groups.items()}
+
+
+def get_development_dependencies(lock: Dict[str, Any]) -> Dict[str, List[Requirement]]:
+    dep_groups = lock.get("tool", {}).get("pdm", {}).get("dev-dependencies", {})
+    return {group: [Requirement(EDITABLE_PATTERN.sub("", dep)) for dep in deps] for group, deps in dep_groups.items()}
+
+
+@dataclass
+class PDMPackage:
+    name: NormalizedName
+    version: Version
+    python_versions: SpecifierSet
+    dependencies: Set[Requirement]
+    files: Set[PackageFile]
+    resolved_dependencies: Set[PackageDependency]
+    extras: Set[str]
+
+    def __post_init__(self):
+        self.extras = set(e.lower() for e in self.extras)
+
+    @property
+    def key(self) -> PackageKey:
+        return PackageKey.from_parts(self.name, self.version)
+
+    def satisfies(self, req: Requirement) -> bool:
+        # The left side is already canonicalized.
+        if self.name != package_canonical_name(req.name):
+            return False
+        # Extras are case-insensitive. The left side is already lower-cased.
+        if not self.extras.issuperset(set(r.lower() for r in req.extras)):
+            return False
+        return req.specifier.contains(self.version)
+
+    def to_lock_package(self) -> Package:
+        dependencies_without_self = sorted(
+            [dep for dep in self.resolved_dependencies if dep.key != self.key], key=lambda p: p.key
+        )
+        return Package(
+            name=self.name,
+            version=self.version,
+            python_versions=str(self.python_versions),
+            dependencies=dependencies_without_self,
+            files=sorted(self.files, key=lambda f: f.name),
+        )
+
+    def merge(self, other: PDMPackage) -> PDMPackage:
+        if (self.name, self.version) != (other.name, other.version):
+            raise ValueError(f"Can only merge packages with the same name and version, not {self.key} and {other.key}")
+        if self.python_versions != other.python_versions:
+            raise ValueError(
+                f"Can only merge packages that depend on the same Python version, not {self.python_versions} and {other.python_versions}"
             )
 
-        self._lockfile_file = lock_file.absolute()
-        self._pyproject_file = project_file.absolute()
-        self._cache_dir = cache_dir
+        merged_dependencies = set(self.dependencies) | set(other.dependencies)
+        merged_files = set(self.files) | set(other.files)
+        merged_resolved_dependencies = set(self.resolved_dependencies) | set(other.resolved_dependencies)
+        merged_extras = set(self.extras) | set(other.extras)
 
-    @property
-    def pyproject_file(self) -> Path:
-        return self._pyproject_file
-
-    @property
-    def cache_dir(self) -> Path:
-        return self._cache_dir
-
-
-def candidate_package_key(candidate: PDMCandidate) -> PackageKey:
-    return PackageKey.from_parts(package_canonical_name(candidate.name), Version(candidate.version))
-
-
-def parse_hash_sha256(hash: str) -> str:
-    assert hash.startswith("sha256:")
-    return hash[7:]
-
-
-def get_pins(
-    project: PDMProject,
-    default_dependencies: bool,
-    dev_dependencies: bool,
-    dependency_groups: List[str],
-) -> Dict[NormalizedName, PackageKey]:
-    pins = {}
-    repository = project.locked_repository
-    try:
-        groups = PDMGroupSelection(
-            project=project,
-            default=default_dependencies,
-            dev=dev_dependencies,
-            groups=dependency_groups,
-        ).all()
-    except PdmUsageError:
-        raise Exception(
-            "Failed to resolve groups."
-            "Most likely because a dev-group is selected"
-            "while dev-dependencies are disabled."
+        return PDMPackage(
+            name=self.name,
+            version=self.version,
+            python_versions=self.python_versions,
+            dependencies=merged_dependencies,
+            files=merged_files,
+            resolved_dependencies=merged_resolved_dependencies,
+            extras=merged_extras,
         )
-    dependencies = []
-    for group in groups:
-        dependencies.extend(project.get_dependencies(group).values())
-
-    for dependency in dependencies:
-        for candidate in repository.find_candidates(dependency):
-            # TODO handle the case where multiple candidates are found
-            pin = package_canonical_name(candidate.name)
-            pins[pin] = candidate_package_key(candidate)
-    return pins
 
 
-def get_packages(project: PDMProject) -> Dict[PackageKey, Package]:
-    packages = {}
-    repository = project.locked_repository
-    # Don't evaluate markers against current environment.
-    repository.ignore_compatibility = True
-
-    for _package in repository.packages.values():
-        # NOTE we need to call `get_dependencies` first, as it also fills `requires_python`
-        _dependencies, _, _ = repository.get_dependencies(_package)
-
-        package_key = candidate_package_key(_package)
-        package_name = package_canonical_name(_package.name)
-        package_version = Version(str(_package.version))
-        package_python_versions = _package.requires_python
-        package_dependencies = []
-        package_files = []
-
-        _package_files = {}
-        hashes = repository.get_hashes(_package)
-        if not hashes:
-            raise Exception(f'package "{package_name}" has not hashes')
-        for hash in hashes:
-            file_url = hash.get("url")
-            _, _, file_name = file_url.rpartition("/")
-            file_hash = hash.get("hash")
-            file_sha256 = parse_hash_sha256(file_hash)
-            if file_name not in _package_files:
-                _package_files[file_name] = (file_sha256, [file_url])
-            else:
-                _file_sha256, _file_urls = _package_files[file_name]
-                if file_sha256 != _file_sha256:
-                    raise Exception(f'package "{package_name}" has conflicting hashes for "{file_name}"')
-                _file_urls.append(file_url)
-
-        for file_name, (file_sha256, file_urls) in _package_files.items():
-            package_files.append(PackageFile(name=file_name, sha256=file_sha256, urls=tuple(file_urls)))
-
-        for _dependency in _dependencies:
-            for _candidate in repository.find_candidates(_dependency):
-                # TODO handle the case where multiple candidates are found
-                if _candidate.name == _package.name:
-                    continue
-                dependency = PackageDependency(
-                    name=package_canonical_name(_candidate.name),
-                    version=Version(_candidate.version),
-                    marker=str(_dependency.marker) if _dependency.marker else "",
-                )
-                package_dependencies.append(dependency)
-
-        # if there are multiple packages with the same name (eg in case of package+extras) merge dependencies into
-        # the main package
-        if package_key in packages:
-            package_dependencies.extend(packages[package_key].dependencies)
-
-        package_dependencies = set(package_dependencies)
-        package_dependencies = sorted(package_dependencies, key=lambda d: d.key)
-
-        package = Package(
-            name=package_name,
-            version=package_version,
-            python_versions=package_python_versions,
-            dependencies=package_dependencies,
-            files=package_files,
-        )
-        packages[package_key] = package
-    return packages
+def parse_file_info(file_info: Dict[str, Any]) -> PackageFile:
+    if "file" in file_info:
+        file_name = file_info["file"]
+        urls = None
+    elif "url" in file_info:
+        url = file_info["url"]
+        _, file_name = urlparse(url).path.rsplit("/", 1)
+        urls = (url,)
+    else:
+        raise AssertionError("file entry has no file or url member")
+    file_hash = file_info["hash"]
+    assert file_hash.startswith("sha256:")
+    return PackageFile(name=file_name, sha256=file_hash[7:], urls=urls)
 
 
 def translate(
     project_file: Path,
     lock_file: Path,
-    default_dependencies: bool,
-    dev_dependencies: bool,
-    dependency_groups: List[str],
+    default_group: bool,
+    optional_groups: List[str],
+    all_optional_groups: bool,
+    development_groups: List[str],
+    all_development_groups: bool,
 ) -> LockSet:
-    with TemporaryDirectory() as cache_dir:
-        project = PDMProject(
-            project_file=project_file,
-            lock_file=lock_file,
-            cache_dir=Path(cache_dir),
+    try:
+        with open(project_file, "rb") as f:
+            project_dict = tomli.load(f)
+    except Exception as e:
+        raise Exception(f"Could not load project file: {project_file}: {e}")
+
+    try:
+        with open(lock_file, "rb") as f:
+            lock_dict = tomli.load(f)
+    except Exception as e:
+        raise Exception(f"Could not load lock file: {lock_file}: {e}")
+
+    lock_version = lock_dict.get("metadata", {}).get("lock_version")
+    if not lock_version:
+        raise LockfileIncompatibleException(f"Lock file at {lock_file} has no version")
+    if Version(lock_version) not in SUPPORTED_LOCK_VERSIONS:
+        raise LockfileIncompatibleException(
+            f"Lock file version {lock_version} not included in {SUPPORTED_LOCK_VERSIONS}"
         )
-        pins = get_pins(
-            project=project,
-            default_dependencies=default_dependencies,
-            dev_dependencies=dev_dependencies,
-            dependency_groups=dependency_groups,
+
+    requirements: List[Requirement] = []
+
+    default_dependencies = get_default_dependencies(project_dict)
+    optional_dependencies = get_optional_dependencies(project_dict)
+    development_dependencies = get_development_dependencies(project_dict)
+
+    if default_group:
+        requirements.extend(default_dependencies)
+
+    if all_optional_groups:
+        optional_groups = list(optional_dependencies)
+
+    if all_development_groups:
+        development_groups = list(development_dependencies)
+
+    for group_name in optional_groups:
+        if group_name not in optional_dependencies:
+            raise Exception(f"Non-existent optional dependency group: {group_name}")
+        requirements.extend(optional_dependencies[group_name])
+
+    for group_name in development_groups:
+        if group_name not in development_dependencies:
+            raise Exception(f"Non-existent development dependency group: {group_name}")
+        requirements.extend(development_dependencies[group_name])
+
+    pinned_package_specs = {}
+    for req in requirements:
+        pin = package_canonical_name(req.name)
+        pinned_package_specs[pin] = req
+
+    distinct_packages = {}
+    # Pull out all Package entries in a pdm-specific model.
+    for lock_pkg in lock_dict.get("package", []):
+        package_listed_name = lock_pkg["name"]
+        package_name = package_canonical_name(package_listed_name)
+        package_version = lock_pkg["version"]
+        package_requires_python = lock_pkg.get("requires_python", "")
+        package_extras = lock_pkg.get("extras", [])
+
+        if package_requires_python == "*":
+            # Special case for all python versions
+            package_requires_python = ""
+
+        dependencies = {Requirement(dep) for dep in lock_pkg.get("dependencies", [])}
+        files = {parse_file_info(f) for f in lock_pkg.get("files", [])}
+
+        package = PDMPackage(
+            name=package_name,
+            version=Version(package_version),
+            python_versions=SpecifierSet(package_requires_python),
+            dependencies=dependencies,
+            files=files,
+            resolved_dependencies=set(),
+            extras=set(package_extras),
         )
-        packages = get_packages(project)
-        return LockSet(
-            packages=packages,
-            pins=pins,
-        )
+        if package.key in distinct_packages:
+            distinct_packages[package.key] = package.merge(distinct_packages[package.key])
+        else:
+            distinct_packages[package.key] = package
+
+    all_packages = distinct_packages.values()
+
+    # Next, group packages by their canonical name
+    packages_by_canonical_name: Dict[str, List[PDMPackage]] = defaultdict(list)
+    for package in all_packages:
+        packages_by_canonical_name[package.name].append(package)
+
+    # And sort the packages by version in descending order (newest first)
+    for package_list in packages_by_canonical_name.values():
+        package_list.sort(key=lambda p: p.version, reverse=True)
+
+    # Next, iterate through each package's dependencies and find the newest one that matches.
+    # Construct a PackageDependency and store it.
+    for package in all_packages:
+        for dep in package.dependencies:
+            dependency_packages = packages_by_canonical_name[package_canonical_name(dep.name)]
+            for dep_pkg in dependency_packages:
+                if dep_pkg.satisfies(dep):
+                    resolved = PackageDependency(
+                        name=dep_pkg.name,
+                        version=dep_pkg.version,
+                        marker=str(dep.marker or ""),
+                    )
+                    package.resolved_dependencies.add(resolved)
+                    break
+            else:
+                raise MismatchedVersionException(
+                    f"Found no packages to satisfy dependency (name={dep.name}, spec={dep.specifier})"
+                )
+
+    pinned_keys = {}
+    for pin, pin_spec in pinned_package_specs.items():
+        pin_packages = packages_by_canonical_name[pin]
+        for pin_pkg in pin_packages:
+            if pin_spec.specifier.contains(pin_pkg.version):
+                pinned_keys[pin] = pin_pkg.key
+                break
+        else:
+            raise MismatchedVersionException(f"Found no packages to satisfy pin (name={pin}, spec={pin_spec})")
+
+    lock_packages = {}
+    for package in all_packages:
+        lock_package = package.to_lock_package()
+        lock_packages[lock_package.key] = lock_package
+
+    return LockSet(
+        packages=lock_packages,
+        pins=pinned_keys,
+    )
 
 
 def main(args: Any) -> None:
@@ -199,10 +281,20 @@ def main(args: Any) -> None:
     lock_set = translate(
         project_file=args.project_file,
         lock_file=args.lock_file,
-        default_dependencies=args.default_dependencies,
-        dev_dependencies=args.dev_dependencies,
-        dependency_groups=args.dependency_groups,
+        default_group=args.default_group,
+        optional_groups=args.optional_group,
+        all_optional_groups=args.all_optional_groups,
+        development_groups=args.development_group,
+        all_development_groups=args.all_development_groups,
     )
+
+    if args.require_static_urls:
+        for pkg in lock_set.packages.values():
+            for file in pkg.files:
+                if not file.urls:
+                    raise LockfileNotStaticException(
+                        "Lock file does not contain static urls. Please use --static-urls when creating the lockfile."
+                    )
 
     with open(output, "w") as f:
         f.write(lock_set.to_json(indent=2))
@@ -226,25 +318,41 @@ def parse_flags() -> Any:
     )
 
     parser.add_argument(
-        "--default",
-        dest="default_dependencies",
+        "--default-group",
         action="store_true",
-        help="Whether to install dependencies fromArgumentParserthe default group.",
+        help="Whether to install dependencies from the default group.",
     )
 
     parser.add_argument(
-        "--dev",
-        dest="dev_dependencies",
-        action="store_true",
-        help="Whether to install dev dependencies.",
-    )
-
-    parser.add_argument(
-        "--group",
-        dest="dependency_groups",
+        "--optional-group",
         action="append",
         default=[],
-        help="Additional groups to install.",
+        help="Optional dependency groups to install.",
+    )
+
+    parser.add_argument(
+        "--all-optional-groups",
+        action="store_true",
+        help="Install all optional dependency groups.",
+    )
+
+    parser.add_argument(
+        "--development-group",
+        action="append",
+        default=[],
+        help="Development dependency groups to install.",
+    )
+
+    parser.add_argument(
+        "--all-development-groups",
+        action="store_true",
+        help="Install all development dependency groups.",
+    )
+
+    parser.add_argument(
+        "--require-static-urls",
+        action="store_true",
+        help="Require that the lock file provide static URLs.",
     )
 
     parser.add_argument(

examples/external_linking/BUILD.bazel
@@ -142,11 +142,6 @@ toolchain(
 
 pycross_pdm_lock_model(
     name = "example_lock_model",
-    dev = True,
-    groups = [
-        "cow",
-        "dev",
-    ],
     lock_file = "pdm.lock",
     project_file = "pyproject.toml",
 )

examples/pdm/BUILD.bazel
@@ -71,12 +71,13 @@ pycross_target_environment(
 
 pycross_pdm_lock_model(
     name = "example_lock_model",
-    dev = True,
-    groups = [
-        "cow",
+    development_groups = [
         "dev",
     ],
     lock_file = "pdm.lock",
+    optional_groups = [
+        "cow",
+    ],
     project_file = "pyproject.toml",
 )
 

pycross/private/BUILD.bazel
@@ -138,11 +138,7 @@ pycross_pdm_lock_model(
 pycross_lock_file(
     name = "rules_pycross_deps",
     out = "updated_pycross_deps_lock.bzl",
-    default_alias_single_version = True,
     disallow_builds = True,
-    local_wheels = [
-        "//third_party/wheels",
-    ],
     lock_model_file = ":rules_pycross_deps_lock_model",
     target_environments = [":rules_pycross_deps_target_env"],
 )

pycross/private/pdm_lock_model.bzl
@@ -11,11 +11,20 @@ def _pycross_pdm_lock_model_impl(ctx):
     if ctx.attr.default:
         args.add("--default")
 
-    if ctx.attr.dev:
-        args.add("--dev")
+    for group in ctx.attr.optional_groups:
+        args.add("--optional-group", group)
 
-    for group in ctx.attr.groups:
-        args.add("--group", group)
+    if ctx.attr.all_optional_groups:
+        args.add("--all-optional-groups")
+
+    for group in ctx.attr.development_groups:
+        args.add("--development-group", group)
+
+    if ctx.attr.all_development_groups:
+        args.add("--all-development-groups")
+
+    if ctx.attr.require_static_urls:
+        args.add("--require-static-urls")
 
     ctx.actions.run(
         inputs = (
@@ -48,18 +57,23 @@ pycross_pdm_lock_model = rule(
         ),
         "default": attr.bool(
             doc = "Whether to install dependencies from the default group.",
-            mandatory = False,
             default = True,
         ),
-        "dev": attr.bool(
-            doc = "Whether to install dev dependencies.",
-            mandatory = False,
-            default = False,
+        "optional_groups": attr.string_list(
+            doc = "List of optional dependency groups to install.",
         ),
-        "groups": attr.string_list(
-            doc = "Select groups of optional-dependencies or dev-dependencies to install.",
-            mandatory = False,
-            default = [],
+        "all_optional_groups": attr.bool(
+            doc = "Install all optional dependencies.",
+        ),
+        "development_groups": attr.string_list(
+            doc = "List of development dependency groups to install.",
+        ),
+        "all_development_groups": attr.bool(
+            doc = "Install all dev dependencies.",
+        ),
+        "require_static_urls": attr.bool(
+            doc = "Require that the lock file is created with --static-urls.",
+            default = True,
         ),
         "_tool": attr.label(
             default = Label("//pycross/private/tools:pdm_translator"),

pycross/private/tools/BUILD.bazel
@@ -74,7 +74,6 @@ py_binary(
     deps = [
         ":args",
         ":lock_model",
-        "@rules_pycross_deps//deps:pdm",
         "@rules_pycross_deps//deps:tomli",
     ],
 )
@@ -113,7 +112,6 @@ py_binary(
         "//pycross/private/tools/crossenv",
         "@rules_pycross_deps//deps:build",
         "@rules_pycross_deps//deps:packaging",
-        "@rules_pycross_deps//deps:python_dotenv",
         "@rules_pycross_deps//deps:tomli",
     ],
 )

pycross/private/tools/poetry_translator.py
@@ -13,7 +13,7 @@ from packaging.utils import InvalidWheelFilename
 from packaging.utils import NormalizedName
 from packaging.utils import parse_sdist_filename
 from packaging.utils import parse_wheel_filename
-from packaging.utils import Version
+from packaging.version import Version
 from poetry.core.constraints.version import parse_constraint
 from poetry.core.constraints.version import Version as PoetryVersion
 from poetry.core.version import markers

commit aaf39757644dca648fe67d0b1bc93f784d4b6c49

fix(tools): pdm_translator whitespace bug
Originally introduced in aaa6873cd0ede9219ae054cc0b29a31a1f555713

Python is the best :^)

pycross/private/tools/pdm_translator.py
@@ -260,10 +260,10 @@ def translate(
                     )
                     package.resolved_dependencies.add(resolved)
                     break
-            else:
-                raise MismatchedVersionException(
-                    f"Found no packages to satisfy dependency (name={dep.name}, spec={dep.specifier})"
-                )
+                else:
+                    raise MismatchedVersionException(
+                        f"Found no packages to satisfy dependency (name={dep.name}, spec={dep.specifier})"
+                    )
 
     pinned_keys: Dict[NormalizedName, PackageKey] = {}
     for pin, pin_spec in pinned_package_specs.items():