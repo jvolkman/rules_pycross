c9a9597b053775c99e88f96db581dd51121f8a1b
Jeremy Volkman <volkman@google.com>
2026-05-08 00:35:08 +0000

    Modernize BCR automation

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -2,18 +2,23 @@
 
 set -o errexit -o nounset -o pipefail
 
-# Set by GH actions, see
-# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
-TAG=${GITHUB_REF_NAME}
+# Argument provided by reusable workflow caller
+TAG=$1
 # The prefix is chosen to match what GitHub generates for source archives
-# This guarantees that users can easily switch from a released artifact to a source archive
-# with minimal differences in their code (e.g. strip_prefix remains the same)
 PREFIX="rules_pycross-${TAG:1}"
 ARCHIVE="rules_pycross-$TAG.tar.gz"
 
 # NB: configuration for 'git archive' is in /.gitattributes
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
+
+# Add generated API docs
+docs="$(mktemp -d)"; targets="$(mktemp)"
+bazel --output_base="$docs" query --output=label --output_file="$targets" 'kind("starlark_doc_extract rule", //...)'
+bazel --output_base="$docs" build --target_pattern_file="$targets"
+tar --create --auto-compress \
+    --directory "$(bazel --output_base="$docs" info bazel-bin)" \
+    --file "$GITHUB_WORKSPACE/${ARCHIVE%.tar.gz}.docs.tar.gz" .
 
 cat << EOF
 See the [changelog](CHANGELOG.md).
@@ -36,7 +41,5 @@ http_archive(
     strip_prefix = "${PREFIX}",
     url = "https://github.com/jvolkman/rules_pycross/releases/download/${TAG}/${ARCHIVE}",
 )
+\`\`\`
 EOF
-
-awk 'f;/--SNIP--/{f=1}' e2e/smoke/WORKSPACE.bazel
-echo "\`\`\`" 
